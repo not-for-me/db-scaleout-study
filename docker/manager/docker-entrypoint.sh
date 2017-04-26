432603c121047ff177a9bb266dd511daca3160da
woojin.joe <woojin.joe@linecorp.com>
2017-04-26 20:55:15 +0900

    modify compose file

--- docker/manager/docker-entrypoint.sh
+++ docker/manager/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "Start Master HA Manager..."
 
+masterha_check_status --conf=/etc/app1.cnf
+
+masterha_check_ssh --conf=/etc/app1.cnf
+
 masterha_manager --conf=/etc/app1.cnf
 
-sleep 60
+masterha_check_status --conf=/etc/app1.cnf
+
+sleep 600
 
 exec "$@";
